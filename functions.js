3014397b33f1267546e64c6eaacd856e40ccdecb
gAltelino <galtelino@gmail.com>
2017-07-11 00:38:51 -0300

    long lat check

--- functions.js
+++ functions.js
@@ -14,6 +14,9 @@ function darkSkyApi(latitude, longitude){
 
     const key = "713da3163474a17662960c7e636c8068" ;
 
+    console.log(latitude);
+    console.log(longitude);
+
     getJSON('https://api.darksky.net/forecast/' + key + '/' + latitude + '/' + longitude, function(weather){
         console.log(weather);
     })
