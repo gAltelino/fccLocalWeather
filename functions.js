b8c048bc505f05e3d8f1e93af5a1a7eccf28fe61
gAltelino <galtelino@gmail.com>
2017-07-11 01:06:11 -0300

    long lat check

--- functions.js
+++ functions.js
@@ -4,7 +4,8 @@ function getWeather(){
 
         navigator.geolocation.getCurrentPosition(function(position){
             darkSkyApi(position.coords.latitude, position.coords.longitude);  
-        })
+        }) 
+
     }else{
         console.log("Geo not avaliable");
     }
@@ -14,11 +15,11 @@ function darkSkyApi(latitude, longitude){
 
     const key = "713da3163474a17662960c7e636c8068" ;
 
-    console.log(latitude);
-    console.log(longitude);
-
-    getJSON('https://api.darksky.net/forecast/' + key + '/' + latitude + '/' + longitude, function(weather){
+    $.getJSON('https://api.darksky.net/forecast/' + key + '/' + latitude + '/' + longitude, function(weather){
         console.log(weather);
+
+       // $('#temperature').html = weather.temperature;
+
     })
 
 }
