a24164b1dbdfaf664f0d9753e0afac30d1487876
gAltelino <galtelino@gmail.com>
2017-07-13 01:43:39 -0300

    updates

--- functions.js
+++ functions.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+    getWeather();
+});
+
+
 function getWeather(){ 
 
     if("geolocation" in navigator){
@@ -10,19 +15,42 @@ function getWeather(){
         console.log("Geo not avaliable");
     }
 }
-
 function darkSkyApi(latitude, longitude){
 
     const key = "713da3163474a17662960c7e636c8068" ;
-    console.log(latitude);
-    console.log(longitude);
+   
+        $.ajax({
+            url: 'https://api.darksky.net/forecast/' + key + '/' + latitude + ',' + longitude,
+            type: 'ǴET',
+            dataType:'JSONP'
+        }
+                )
+        .done(function(data){
+        $("#temperature").html(JSON.stringify(data.currently.temperature));
 
-    $.getJSON('https://api.darksky.net/forecast/' + key + '/' + latitude + ',' + longitude, function(weather){
-        console.log(weather);
-
-       // $('#temperature').html = weather.temperature;
-
-    })
-
+        console.log(data.currently.icon);
+        //'<div id="icon"></div>'
+        })                
 }
-  
+
+function swtichCelsiusfahrenheit(){
+    
+    if($("#switcher").text() === '°C'){
+        $("#switcher").html('°F')
+        temperatureToFahrenheit();
+    }else{
+        $("#switcher").html('°C')
+        temperatureToCelcius();
+    }}
+
+function temperatureToCelcius(){
+    var temperature = $("#temperature").text();
+    temperature = (temperature - 32) / 1.8;
+    $("#temperature").html(temperature.toFixed(0));
+}   
+
+function temperatureToFahrenheit(){
+var temperature = $("#temperature").text();
+    temperature = (temperature * 1.8) + 32;
+    $("#temperature").html(temperature.toFixed(0));
+}
